914de7818c8b0a88b977a2126c3024c599eb1539
Eddie Espinoza <eddie.espinoza@prosumia.la>
2025-01-13 13:00:11 -0300

    updated app for the option used with db

--- {{cookiecutter.project_name}}/backend/app.py
+++ {{cookiecutter.project_name}}/backend/app.py
@@ -2,6 +2,10 @@ from flask import Flask, jsonify
 from flask_jwt_extended import JWTManager
 from flask_marshmallow import Marshmallow
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
+{%- if cookiecutter.use_db == "yes" %}
+from sqlalchemy import text
+from db.database import db
+{%- endif %}
 
 from core.config import APP_CONFIG
 
@@ -14,6 +18,9 @@ def create_app():
     app.config.from_object(APP_CONFIG)
     app.json.sort_keys = False
     
+    {%- if cookiecutter.use_db == "yes" %}
+    db.init_app(app)
+    {%- endif %}
     ma.init_app(app)
     JWTManager(app)
 
@@ -31,6 +38,16 @@ def create_app():
         }
         return jsonify(info_data), 200
     
+    {%- if cookiecutter.use_db == "yes" %}
+    @app.route("/test_db_connection")
+    def test_db_connection():
+        try:
+            db.session.execute(text('SELECT 1'))
+            return jsonify("Database connection successful!"), 200
+        except Exception as e:
+            import traceback
+            return jsonify(f"Database connection failed: {e}", traceback.format_exc())
+    {%- endif %}
     return app
 
     
