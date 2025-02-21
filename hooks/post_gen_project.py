d5321dcf828a9a02fc5a31041e1ba4da484b558e
Eddie Espinoza <eddie.espinoza@prosumia.la>
2025-02-21 16:37:30 -0300

    Refactor post gen project hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import json
 import secrets
 import shutil
 
@@ -34,7 +35,24 @@ def generate_secret_key():
 secret_key = generate_secret_key()
 token_secret_key = generate_secret_key()
 
-cookiecutter.update({
-    "secret_key": secret_key,
-    "token_secret_key": token_secret_key
-})
+# Ruta al archivo cookiecutter.json
+cookiecutter_json_path = os.path.join(os.getcwd(), 'cookiecutter.json')
+
+# Actualizar el archivo cookiecutter.json con las claves generadas
+if os.path.exists(cookiecutter_json_path):
+    with open(cookiecutter_json_path, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+    # Asegurarnos de que las claves secretas se agreguen al diccionario
+    data['secret_key'] = secret_key
+    data['token_secret_key'] = token_secret_key
+
+    with open(cookiecutter_json_path, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=4)
+
+    # Mostrar las claves generadas
+    print(f"Secret Key: {secret_key}")
+    print(f"Token Secret Key: {token_secret_key}")
+else:
+    print(f"ERROR: No se encontró el archivo {cookiecutter_json_path}")
+    sys.exit(1)
