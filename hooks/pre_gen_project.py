53f5789fd40ffb34bd9e58ebb96657c62d429881
Eddie Espinoza <eddie.espinoza@prosumia.la>
2025-02-21 14:09:47 -0300

    Add pre hooks

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -4,8 +4,11 @@ import secrets
 def generate_secret_key():
     return secrets.token_urlsafe(32)
 
+secret_key = generate_secret_key()
+token_secret_key = generate_secret_key()
+
 cookiecutter.update({
-    "secret_key": generate_secret_key(),
-    "token_secret_key": generate_secret_key()
+    "secret_key": secret_key,
+    "token_secret_key": token_secret_key
 })
 
