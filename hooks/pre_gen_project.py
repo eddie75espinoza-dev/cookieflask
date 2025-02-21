bd2555a6775ebcdcb8f04ce3f436984bba87e81e
Eddie Espinoza <eddie.espinoza@prosumia.la>
2025-02-21 15:31:15 -0300

    Refactor pre hooks

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,8 +1,5 @@
 import secrets
 
 
-def generate_secret_key():
-    return secrets.token_urlsafe(32)
-
-{{ cookiecutter.update({"secret_key": generate_secret_key() })}}
-{{ cookiecutter.update({"token_secret_key": generate_secret_key() })}}
+{{ cookiecutter.update({"secret_key": secrets.token_urlsafe(32)}) }}
+{{ cookiecutter.update({"token_secret_key": secrets.token_urlsafe(32)}) }}
