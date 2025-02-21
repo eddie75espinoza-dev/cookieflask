47b666f55b27c510c9198295ad15ed8cc43981ef
Eddie Espinoza <eddie.espinoza@prosumia.la>
2025-02-21 14:07:41 -0300

    Refactor json cookiecutter

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,18 +1,11 @@
 import secrets
 
-# Generar claves seguras
+
 def generate_secret_key():
     return secrets.token_urlsafe(32)
 
-# Asignar las claves generadas a los valores del contexto de Cookiecutter
-context = {
+cookiecutter.update({
     "secret_key": generate_secret_key(),
     "token_secret_key": generate_secret_key()
-}
+})
 
-# Actualizar el contexto global de Cookiecutter
-def update_cookiecutter_context(context):
-    for key, value in context.items():
-        globals()["cookiecutter"][key] = value
-
-update_cookiecutter_context(context)
