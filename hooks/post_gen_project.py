2fd2a0ebcb85fb58a1c4e157b81e692ae8f59e10
Eddie Espinoza <eddie.espinoza@prosumia.la>
2025-02-21 17:59:12 -0300

    Refactor post gen project hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -8,7 +8,8 @@ def generate_secret_key():
     return secrets.token_urlsafe(32)
 
 use_db = '{{cookiecutter.use_db}}'
-secret_key = generate_secret_key()
+secret_key = '{{cookiecutter.secret_key}}'
+new_secret_key = generate_secret_key()
 env_dev_file = os.path.join(os.getcwd(), '.env.dev')
 env_prod_file = os.path.join(os.getcwd(), '.env.prod')
 
@@ -34,28 +35,28 @@ if use_db == "no":
         print("ERROR: cannot delete models path %s" % models_path)
         sys.exit(1)
 
+if secret_key != "If you don't change it, it will be changed.":
+    if os.path.exists(env_dev_file):
+        with open(env_dev_file, 'r') as dev_file:
+            content = dev_file.read()
+        content = content.replace("If you don't change it, it will be changed.", new_secret_key)
+        
+        with open(env_dev_file, 'w') as dev_file:
+            dev_file.write(content)
+        print(f"✅ Secret keys updated in {env_dev_file}")
 
-if os.path.exists(env_dev_file):
-    with open(env_dev_file, 'r') as dev_file:
-        content = dev_file.read()
-    content = content.replace('_secret_key_to_replace_', secret_key)
-    
-    with open(env_dev_file, 'w') as dev_file:
-        dev_file.write(content)
-    print(f"✅ Secret keys updated in {env_dev_file}")
-
-else:
-    print(f"⚠️ File {env_dev_file} not found.")
+    else:
+        print(f"⚠️ File {env_dev_file} not found.")
 
 
-if os.path.exists(env_prod_file):
-    with open(env_prod_file, 'r') as prod_file:
-        content = prod_file.read()
-    content = content.replace('_secret_key_to_replace_', secret_key)
-    
-    with open(env_prod_file, 'w') as prod_file:
-        prod_file.write(content)
-    print(f"✅ Secret keys updated in {env_prod_file}")
+    if os.path.exists(env_prod_file):
+        with open(env_prod_file, 'r') as prod_file:
+            content = prod_file.read()
+        content = content.replace("If you don't change it, it will be changed.", new_secret_key)
+        
+        with open(env_prod_file, 'w') as prod_file:
+            prod_file.write(content)
+        print(f"✅ Secret keys updated in {env_prod_file}")
 
-else:
-    print(f"⚠️ File {env_prod_file} not found.")
+    else:
+        print(f"⚠️ File {env_prod_file} not found.")
